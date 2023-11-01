1d96693e8e97ff62434fba565524afec5cf30b98
Muhammedjazimtk <77974989+Muhammedjazimtk@users.noreply.github.com>
2023-11-01 16:24:05 +0530

    added color changig on click

--- src/App.jsx
+++ src/App.jsx
@@ -14,33 +14,45 @@ function App() {
   }, []);
 
   function handleClick() {
+    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
     axios.get("https://api.quotable.io/random").then((response) => {
       document.getElementById("qoute").innerHTML = response.data.content;
       document.getElementById("author").innerHTML = response.data.author;
     });
-    const element = document.getElementsByClassName("change");
 
-    document.body.style.color = " black";
+    const elements = document.getElementsByClassName("change");
+    const c = document.getElementsByClassName("c");
+
+    for (let i = 0; i < c.length; ++i) {
+      c[i].style.backgroundColor = "#" + randomColor;
+    }
+
+    for (let i = 0; i < elements.length; ++i) {
+      elements[i].style.color = "#" + randomColor;
+    }
   }
   return (
-    <div className="flex flex-col gap-4 justify-center items-center bg-green-300 h-screen w-full change">
-      <div className="flex flex-col bg-white rounded-sm p-14 h-auto w-[550px] gap-4">
+    <div className="flex flex-col gap-4 justify-center items-center bg-green-300 h-screen w-full c  ">
+      <div className="w-[300px] flex flex-col bg-white rounded-sm p-14 h-auto md:w-[550px] gap-4">
         <div>
           <p
             id="qoute"
-            className="text-2xl font-medium text-center text-green-300"
+            className=" text-md md:text-2xl font-medium text-center text-green-300 change"
           ></p>
         </div>
-        <p id="author" className="text-right font-light text-green-300 "></p>
-        <div className="flex items-center   justify-between ">
+        <p
+          id="author"
+          className="text-sm md:text-base text-right font-light text-green-300  change"
+        ></p>
+        <div className="flex flex-col h-[100px] justify-around md:flex-row  items-center   md:justify-between  ">
           <div className="flex gap-1  ">
-            <div className="flex justify-center items-center h-[40px] w-[40px] bg-green-300">
+            <div className="flex justify-center items-center h-[40px] w-[40px] bg-green-300 c">
               <InstagramIcon
                 className=" text-white rounded-sm  "
                 fontSize="small"
               />
             </div>
-            <div className="flex justify-center items-center h-[40px] w-[40px] bg-green-300">
+            <div className="flex justify-center items-center h-[40px] w-[40px] bg-green-300 c">
               <TwitterIcon
                 className=" text-white rounded-sm "
                 fontSize="small"
@@ -50,7 +62,7 @@ function App() {
           <button
             id="btn"
             onClick={handleClick}
-            className="bg-green-300 text-white font-normal py-2 px-3 text-sm rounded-sm "
+            className="bg-green-300 text-white font-normal py-2 px-3 text-sm rounded-sm c "
           >
             New quote
           </button>
